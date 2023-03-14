6a8e95afd1399446a99dd7626ff7521e2e1f7c8a
ritik jha <coolritikjha46@gmail.com>
2023-03-15 01:45:22 +0530

    adding cors

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-const express=require('express')
-const dotenv=require('dotenv')
-const {connectDB}=require('./database/db')
-const userRoutes=require('./routes/userRoutes')
-const solutionRoutes=require('./routes/solutionRoutes')
-const teamRoutes=require('./routes/teamRoutes')
-const problemRoutes=require('./routes/problemRoutes')
-const app=express()
-dotenv.config()
-const PORT=5000
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
-app.use(express.json())
-connectDB()
+const express = require("express");
+const dotenv = require("dotenv");
+const { connectDB } = require("./database/db");
+const userRoutes = require("./routes/userRoutes");
+const solutionRoutes = require("./routes/solutionRoutes");
+const teamRoutes = require("./routes/teamRoutes");
+const problemRoutes = require("./routes/problemRoutes");
+const cors = require("cors");
+const options = {
+    origin : ['http://localhost:3000','https://houseofhackers.vercel.app']
+}
+const app = express();
+dotenv.config();
+const PORT = 5000;
+
+app.use(express.json());
+connectDB();
 app.use("/api/user", userRoutes);
 app.use("/api/team", teamRoutes);
-app.use("/api/submission",solutionRoutes);
-app.use("/api/problem",problemRoutes);
+app.use("/api/submission", solutionRoutes);
+app.use("/api/problem", problemRoutes);
 
-
-
-
-
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
