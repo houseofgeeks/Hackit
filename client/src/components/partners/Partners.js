73221a4ea7b4a29d39b31c25e12a07b1fd98aaac
ritik jha <coolritikjha46@gmail.com>
2023-03-26 14:51:43 +0530

    coorganizers added

--- client/src/components/partners/Partners.js
+++ client/src/components/partners/Partners.js
@@ -15,11 +15,6 @@ const Partners = () => {
       img: threeway,
     },
     {
-      type: "Coorganizers",
-      name: "Dev Dungeon",
-      img: devdungeon,
-    },
-    {
       type: "Platform Partner",
       name: "Hack 2 Skill",
       img: hack2skill,
@@ -34,11 +29,28 @@ const Partners = () => {
         {sponsorsData.map((sponsors) => {
           return (
             <div className="my-6 flex flex-col items-center">
-              <h1 className="text-yellow-600 md:w-[450px] w-[100%] text-center bg-[rgba(255,255,255,0.1)] font-semibold font-[Urbanist] text-2xl px-6 py-2 rounded-md">{sponsors.type}</h1>
-              <img className="md:max-w-[300px] max-w-[250px] mt-4" src={sponsors.img} alt="" />
+              <h1 className="text-yellow-600 md:w-[450px] w-[100%] text-center bg-[rgba(255,255,255,0.1)] font-semibold font-[Urbanist] text-2xl px-6 py-2 rounded-md">
+                {sponsors.type}
+              </h1>
+              <img
+                className="md:max-w-[300px] max-w-[250px] mt-4"
+                src={sponsors.img}
+                alt=""
+              />
             </div>
           );
         })}
+      </div>
+
+      <h1 className="text-center mb-8 mt-8 font-[Varela] text-[#ee2a7b] font-bold text-5xl">
+        Coorganizers
+      </h1>
+      <div className="my-6 flex flex-col items-center">
+        <img
+          className="md:max-w-[300px] max-w-[250px] mt-4"
+          src={devdungeon}
+          alt=""
+        />
       </div>
     </div>
   );
